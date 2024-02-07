8e75af51493cd34a54be55274328385c4858e46e
Diego Romero <draketromero@gmail.com>
2024-02-06 21:06:27 -0600

    Version en ingles configurada y agregada

---
+++ src/interfaces/index.ts
@@ -0,0 +1,16 @@
+export { type Resume } from "./cv.interface";
+export { type Award } from "./cv.interface";
+export { type Basics } from "./cv.interface";
+export { type Location } from "./cv.interface";
+export { type Profile } from "./cv.interface";
+export { type Certificate } from "./cv.interface";
+export { type Education } from "./cv.interface";
+export {type Interest } from "./cv.interface";
+export {type Language } from "./cv.interface";
+export {type Project } from "./cv.interface";
+export {type Publication } from "./cv.interface";
+export {type Reference } from "./cv.interface";
+export {type Skill } from "./cv.interface";
+export {type Level } from "./cv.interface";
+export {type Volunteer } from "./cv.interface";
+
